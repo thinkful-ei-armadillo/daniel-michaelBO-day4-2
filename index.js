e2b3ec6d544dbddfa3e104d1fc30e7feab92fe0e
Daniel Ors <do7811@gmail.com>
2019-01-17 18:06:08 -0500

    completed shopping list challenge

--- index.js
+++ index.js
@@ -3,33 +3,36 @@
 'use strict';
 
 $(function () {
+
+  // adding to list functionality
   $('#js-shopping-list-form').on('submit', function(event) {
     
     event.preventDefault();
 
-    $('ul').append('<li>' + $('#shopping-list-entry').val() + '</li>');
+    $('ul').append('<li><span class="shopping-item">' +
+                      $('#shopping-list-entry').val() + `</span>
+                      <div class="shopping-item-controls">
+                        <button class="shopping-item-toggle">
+                          <span class="button-label">check</span>
+                        </button>
+                        <button class="shopping-item-delete">
+                          <span class="button-label">delete</span>
+                        </button>
+                      </div>
+                    </li>`
+    );
+  });
+
+  // adding checking functionality
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+
+    $(event.currentTarget).parent('div').parent('li').children('.shopping-item').toggleClass('shopping-item__checked');
 
   });
-  $('.shopping-item-toggle').on('click', function(event) {
-    event.preventDefault();
-    console.log('Test');
-    if ( $('.shopping-item').hasClass('shopping-item__checked') === false ) {  
-      $('.shopping-item-toggle').closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
-    }
-    else {
-      $('.shopping-item-toggle').closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
-    }
-  }
-  );
+
+  // adding delete item functionality
   $('ul').on('click', '.shopping-item-delete', function(event) {
     event.preventDefault();
     $(this).closest('li').remove();
   });
 });
-
-// function main() {
-
-// }
-
-
-// $(main);
