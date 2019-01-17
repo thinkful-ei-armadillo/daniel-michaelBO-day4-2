18deaae044d6cd6c33be01ddb0c50c9394866012
Daniel Ors <do7811@gmail.com>
2019-01-17 17:41:35 -0500

    shopping list beta

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
+/* eslint-disable no-console */
 /* global $ */
 'use strict';
 
 $(function () {
   $('#js-shopping-list-form').on('submit', function(event) {
+    
     event.preventDefault();
 
     $('ul').append('<li>' + $('#shopping-list-entry').val() + '</li>');
 
-    // and reference to all other bulbs const otherBulbs = $('.js-lightbulb').not(targetBulb);
-    
-    
-
-    $('.shopping-item-toggle').on('click', function(event) {
-      console.log('Test');
-    $('button').closest('li').find('span').toggleClass('.shopping-item__checked');
-    
-    }
-    
-   
-
-    );
-
-
   });
+  $('.shopping-item-toggle').on('click', function(event) {
+    event.preventDefault();
+    console.log('Test');
+    if ( $('.shopping-item').hasClass('shopping-item__checked') === false ) {  
+      $('.shopping-item-toggle').closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+    }
+    else {
+      $('.shopping-item-toggle').closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+    }
+  }
+  );
   $('ul').on('click', '.shopping-item-delete', function(event) {
+    event.preventDefault();
     $(this).closest('li').remove();
-   });
+  });
 });
 
 // function main() {
