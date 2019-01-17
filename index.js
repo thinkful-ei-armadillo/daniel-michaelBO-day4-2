7a2cfda2a12328196986561c4429fe1aa4cd0ddb
Michael Bonner <michaelb@thinkful.com>
2019-01-17 17:16:23 -0500

    updated shopping list

--- index.js
+++ index.js
@@ -2,15 +2,30 @@
 'use strict';
 
 $(function () {
-  $('button').on( 'click', function(event) {
+  $('#js-shopping-list-form').on('submit', function(event) {
     event.preventDefault();
 
-    $('ul').append('<li>' + $('#shopping-list-entry') + '</li>' + '<span> .shopping-item-toggle' + '.shopping-item-delete' );
+    $('ul').append('<li>' + $('#shopping-list-entry').val() + '</li>');
+
+    // and reference to all other bulbs const otherBulbs = $('.js-lightbulb').not(targetBulb);
+    
+    
+
+    $('.shopping-item-toggle').on('click', function(event) {
+      console.log('Test');
+    $('button').closest('li').find('span').toggleClass('.shopping-item__checked');
+    
+    }
+    
+   
+
+    );
+
 
   });
-  // $('ul').on('click', '.shopping-item-delete', function(event) {
-  //   this.remove();
-  // });
+  $('ul').on('click', '.shopping-item-delete', function(event) {
+    $(this).closest('li').remove();
+   });
 });
 
 // function main() {
